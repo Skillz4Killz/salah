21e05f348e456793d7154c69fd9442d87d88bec6
Skillz <skillz@discord.gg/ddeno>
2023-04-27 09:45:43 -0500

    fix: cleaner ayah component

--- components/Ayah.tsx
+++ components/Ayah.tsx
@@ -12,17 +12,28 @@ export function Ayah(props: AyahProps) {
   if (!ayah) return null;
 
   return (
-    <div>
-      {/* Arabic Text */}
+    <div className="flex items-center justify-center px-5 py-5">
+      <div className="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800 max-w-lg">
+        <div className="w-full mb-10">
+          <div className="text-3xl text-indigo-500 text-left leading-tight h-3">
+            “
+          </div>
+          <p className="text-sm text-gray-600 text-center px-5">{ayah.text}</p>
+          <div className="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">
+            ”
+          </div>
+        </div>
+        {/* TODO: arabic - add arabic text */}
+        <p className="text-xs text-gray-500 text-center">
+          {ayah.transliteration}
+        </p>
 
-      {/* English Translation */}
-      <p className="text-blue-500">{ayah.text}</p>
-      {/* English Transliteration */}
-      <p className="text-blue-300 italic">{ayah.transliteration}</p>
-      {/* Surah & Ayah */}
-      <p className="text-red-300 italic">
-        Surah {props.surah} <span>Ayah {props.ayah}</span>
-      </p>
+        <div className="w-full">
+          <p className="text-md text-indigo-500 font-bold text-center">
+            Surah {props.surah} Ayah {props.ayah}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
